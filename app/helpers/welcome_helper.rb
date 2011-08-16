c3f79c32414d5a22ad314c72321b61442355b804
Roman Simecek <roman.simecek@cyt.ch>
2011-08-16 13:02:41 +0200

    Welcome controller copied from wikisigns.

---
+++ app/helpers/welcome_helper.rb
@@ -0,0 +1,16 @@
+module WelcomeHelper
+  def overview_navigation
+    [
+      {:href => cms_slug_prefix + 'liveart/liveart-ankenwaage', :coords => '176,0,265,28', :title => 'liveart'},
+      {:href => cms_slug_prefix + 'art/sonnwende', :coords => '342,25,488,54', :title => 'art collection'},
+      {:href => cms_slug_prefix + 'sonstiges', :coords => '155,213,265,241', :title => 'sonstiges'},
+      {:href => cms_slug_prefix + 'biographie', :coords => '401,222,521,248', :title => 'biographie'}
+    ]
+  end
+
+  private
+
+  def cms_slug_prefix
+    '/de/pintosign/'
+  end
+end
