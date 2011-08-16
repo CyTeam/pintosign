3a5a5e4f9c0c65fdef1076c11a5d2ad23e02d71e
Roman Simecek <roman.simecek@cyt.ch>
2011-08-16 12:40:56 +0200

    Initial rails 3.1 commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pintosign::Application.config.secret_token = 'cf36ba3ee2d97eaea7f759900f080b81cab2480d2b829fd1741763592e860d2c082adb99c015ab44c10fbb0ae71d141f903e7253fbda7d6b70117665ca0b3007'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Pintosign::Application.config.session_store :cookie_store, key: '_pintosign_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Pintosign::Application.config.session_store :active_record_store
