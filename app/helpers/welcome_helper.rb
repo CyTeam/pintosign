bdbae182adfc8cb3eca3647868613265231c0257
Roman Simecek <roman.simecek@cyt.ch>
2013-05-23 17:49:37 +0200

    Restore the old start page.

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -4,8 +4,7 @@ module WelcomeHelper
       {:href => cms_slug_prefix + 'liveart/liveart-ankenwaage', :coords => '176,0,265,28',    :title => 'liveart'},
       {:href => cms_slug_prefix + 'art/sonnwende',              :coords => '342,25,488,54',   :title => 'art collection'},
       {:href => cms_slug_prefix + 'info/presse',                :coords => '155,213,265,241', :title => 'sonstiges'},
-      {:href => cms_slug_prefix + 'biographie',                 :coords => '401,222,521,248', :title => 'biographie'},
-      {:href => cms_slug_prefix + 'pinto-auf-reisen',           :coords => '311,107,406,187', :title => 'pinto auf reisen'}
+      {:href => cms_slug_prefix + 'biographie',                 :coords => '401,222,521,248', :title => 'biographie'}
     ]
   end
 
