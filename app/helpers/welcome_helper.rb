c4cef7768c9e8ff4c5f1873eaae27b47729628d7
Roman Simecek <roman@good2go.ch>
2014-08-24 16:37:32 +0200

    Update the links from the welcome view.

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,16 +1,12 @@
 module WelcomeHelper
+
   def overview_navigation
     [
-      {:href => cms_slug_prefix + 'liveart/liveart-ankenwaage', :coords => '176,0,265,28',    :title => 'ausstellungen'},
-      {:href => cms_slug_prefix + 'art/sonnwende',              :coords => '342,25,488,54',   :title => 'art collection'},
-      {:href => cms_slug_prefix + 'info/presse',                :coords => '155,213,265,241', :title => 'sonstiges'},
-      {:href => cms_slug_prefix + 'biographie',                 :coords => '401,222,521,248', :title => 'biographie'}
+      {href: '/de/ausstellungen/liveart-future', coords: '176,0,265,28',    title: 'ausstellungen'},
+      {href: '/de/art/sonnwende',                coords: '342,25,488,54',   title: 'art collection'},
+      {href: '/de/info/presse',                  coords: '155,213,265,241', title: 'sonstiges'},
+      {href: '/de/biographie',                   coords: '401,222,521,248', title: 'biographie'}
     ]
   end
 
-  private
-
-  def cms_slug_prefix
-    '/de/pintosign/'
-  end
 end
