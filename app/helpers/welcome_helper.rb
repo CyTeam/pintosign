2a7def338359fa07ed49cdbaebef0f95212e021b
Roman Simecek <roman@good2go.ch>
2014-10-05 21:48:49 +0200

    Get the map links through the kuhsaft page model.

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -2,11 +2,27 @@ module WelcomeHelper
 
   def overview_navigation
     [
-      {href: '/de/ausstellungen/liveart-future', coords: '176,0,265,28',    title: 'ausstellungen'},
-      {href: '/de/art/sonnwende',                coords: '342,25,488,54',   title: 'art collection'},
-      {href: '/de/info/presse',                  coords: '155,213,265,241', title: 'sonstiges'},
-      {href: '/de/biographie',                   coords: '401,222,521,248', title: 'biographie'}
+      {href: ausstellungen_page.link,  coords: '176,0,265,28',    title: ausstellungen_page.title},
+      {href: art_collection_page.link, coords: '342,25,488,54',   title: art_collection_page.title},
+      {href: sonstiges_page.link,      coords: '155,213,265,241', title: sonstiges_page.title},
+      {href: biographie_page.link,     coords: '401,222,521,248', title: biographie_page.title},
     ]
+  end
+
+  def ausstellungen_page
+    @ausstellungen_page ||= Kuhsaft::Page.where(title_de: 'ausstellungen').first
+  end
+
+  def art_collection_page
+    @art_collection_page ||= Kuhsaft::Page.where(title_de: 'art').first
+  end
+
+  def sonstiges_page
+    @sonstiges_page ||= Kuhsaft::Page.where(title_de: 'info').first
+  end
+
+  def biographie_page
+    @biographie_page ||= Kuhsaft::Page.where(title_de: 'biographie').first
   end
 
 end
