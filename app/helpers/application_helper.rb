cc6561646a2bc4d189312cf94840005a1ff4780f
Roman Simecek <roman@good2go.ch>
2015-01-14 14:00:31 +0100

    Create the welcome page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,4 +29,32 @@ module ApplicationHelper
     ]
   end
 
+  def overview_navigation
+    [
+      {href: ausstellungen_page.nested_path,  coords: '176,0,325,28',    title: ausstellungen_page.path},
+      {href: art_collection_page.nested_path, coords: '342,25,488,54',   title: art_collection_page.path},
+      {href: sonstiges_page.nested_path,      coords: '155,213,265,241', title: sonstiges_page.path},
+      {href: biographie_page.nested_path,     coords: '401,222,521,248', title: biographie_page.path},
+    ]
+  end
+
+  private
+
+  def ausstellungen_page
+    @ausstellungen_page ||= Refinery::Page.by_title('ausstellungen').first
+  end
+
+  def art_collection_page
+    @art_collection_page ||= Refinery::Page.by_title('art').first
+  end
+
+  def sonstiges_page
+    @sonstiges_page ||= Refinery::Page.by_title('biographie').first
+  end
+
+  def biographie_page
+    @biographie_page ||= Refinery::Page.by_title('info').first
+  end
+
+
 end
--- config/initializers/refinery/pages.rb
+++ config/initializers/refinery/pages.rb
@@ -46,9 +46,9 @@ Refinery::Pages.configure do |config|
   # Set this to true to fully expand the page hierarchy in the admin
   # config.auto_expand_admin_tree = true
 
-  # config.layout_template_whitelist = ["application"]
+  config.use_layout_templates      = true
+  config.layout_template_whitelist = %w[application intro]
 
-  # config.use_layout_templates = false
 
   # config.page_title = {:chain_page_title=>false, :ancestors=>{:separator=>" | ", :class=>"ancestors", :tag=>"span"}, :page_title=>{:class=>nil, :tag=>nil, :wrap_if_not_chained=>false}}
 
