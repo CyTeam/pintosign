e6f54718cc0622c1244907ef2f7c888dd7df4a39
Roman Simecek <roman@good2go.ch>
2015-02-13 13:22:54 +0100

    Add menu title with a bullet point.

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,21 +22,25 @@ ausstellungen = Refinery::Page.create!(
 univers = Refinery::Page.create!(
   title:  "universum von pinto",
   parent: ausstellungen,
+  menu_title: "• universum von pinto",
 )
 
 ankenwaage = Refinery::Page.create!(
   title:  "ankenwaage",
   parent: ausstellungen,
+  menu_title: "• ankenwaage",
 )
 
 potz = Refinery::Page.create!(
   title:  "potz 1000",
   parent: ausstellungen,
+  menu_title: "• potz 1000",
 )
 
 ue50art = Refinery::Page.create!(
   title:  "ue50 art",
   parent: ausstellungen,
+  menu_title: "• ue50 art",
 )
 
 art = Refinery::Page.create!(
@@ -48,21 +52,25 @@ art = Refinery::Page.create!(
 rigi = Refinery::Page.create!(
   title:  "rigi",
   parent: art,
+  menu_title: "• rigi",
 )
 
 sonnwende = Refinery::Page.create!(
   title:  "sonnwende",
   parent: art,
+  menu_title: "• sonnwende",
 )
 
 wikisigns_digital = Refinery::Page.create!(
   title:  "wikisigns digital",
   parent: art,
+  menu_title: "• wikisigns digital",
 )
 
 wikisigns_analog = Refinery::Page.create!(
   title:  "wikisigns analog",
   parent: art,
+  menu_title: "• wikisigns analog",
 )
 
 biographie = Refinery::Page.create!(
@@ -79,9 +87,11 @@ info = Refinery::Page.create!(
 impressum = Refinery::Page.create!(
   title:  "impressum",
   parent: info,
+  menu_title: "• impressum",
 )
 
 presse = Refinery::Page.create!(
   title:  "presse",
   parent: info,
+  menu_title: "• presse",
 )
