a74c280b94360142eb2c6de55024df46cb4e2765
Roman Simecek <roman@good2go.ch>
2015-02-16 13:11:36 +0100

    Initialize the js behaviour of the gallery.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,11 @@
 //= require turbolinks
 //= require refinery/site_bar
 //= require font
+//= require jquery.fancybox
+//= require_self
+
+$(document).ready(function() {
+  if ($('#gallery').length > 0) {
+    $('#gallery li a').fancybox()
+  }
+})
