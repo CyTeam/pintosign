5e808dab2c7fda50e31e812b2d31f5aade51aa28
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2015-05-02 18:30:10 +0200

    Add system directory for cap sync.

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :user, "deployer"                               # The server's user for depl
 set :shared_children, shared_children + ['tmp/sockets']
 
 # Sync directories
-set :sync_directories, ['uploads']
+set :sync_directories, ['uploads', 'system']
 set :sync_backups, 3
 
 # Configuration
